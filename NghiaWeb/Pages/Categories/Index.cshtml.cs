2354142f86ccac9e2afd3b8c92e86a76f3a7fad4
lmnghia911 <lmnghia911@gmail.com>
2022-08-21 23:00:27 +0700

    razor page

---
+++ NghiaWeb/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NghiaWeb.Data;
+using NghiaWeb.Model;
+
+namespace NghiaWeb.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IEnumerable<Category> Categories { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            Categories = _db.Category;
+        }
+    }
+}
